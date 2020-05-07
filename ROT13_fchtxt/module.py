3b737f7c43bbec5c8fdbbf77083df381e5e35666
BROQUE Romann <romannbroque@gmail.com>
2020-05-07 10:22:48 +0200

    Cleaning of module.py

--- ROT13_fchtxt/module.py
+++ ROT13_fchtxt/module.py
@@ -1,27 +1,34 @@
 # -*- coding: utf-8 -*-
 
 def crypting(liste,sentence,i,n,rank_sentence,final):
-     
-     while n < i:                            
-          
-          half = int(len("".join(liste))/2)                 ##half correspond a la moitie de liste
-          sentence[n] = liste.index(sentence[n])+1          ##Attribue a chaque caractere de l'input un rang dans la liste
-          rank_sentence.append(sentence[n])           
-          
-          if sentence[n]+half < (2*half) or sentence[n]+half == (2*half):
-               
-               final.append(liste[sentence[n]+(half-1)])      ##Ajoute half au rang de l'input
-               n+=1
-          
-          elif sentence[n]+half > (2*half):                   ##Modulo 2half pour que le rang ne soit pas superieur a 2half
-               
-               sentence[n] = (sentence[n]+half) % (2*half)
-               final.append(liste[sentence[n]-1])
-               n+=1
-     
-     print("Your message contain",i,"characters\n")
-     
+
+	 while n < i :
+
+		half = int(len("".join(liste))/2)
+
+		##Attribute to every single character of the input a rank in the list
+
+		sentence[n] = liste.index(sentence[n])+1
+		rank_sentence.append(sentence[n])
+
+	if sentence[n]+half < (2*half) or sentence[n]+half == (2*half):
+ 
+		##Add half to the rank of the input
+
+		final.append(liste[sentence[n]+(half-1)])     
+		n+=1
+	
+	elif sentence[n]+half > (2*half):
+
+		##Modulo 2half : rank can't be greater than 2half
+
+		sentence[n] = (sentence[n]+half) % (2*half)
+		final.append(liste[sentence[n]-1])
+		n+=1
+
+	print("Your message contain",i,"characters\n")
+
 def put(sentence):
-        sentence = input("What's your message ?\n")
-        return sentence
-       
+
+	sentence = input("What's your message ?\n")
+	return sentence
